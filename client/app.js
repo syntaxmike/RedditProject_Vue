3e5ff2589fbfe5c68f5a8622de6624cf690167ef
Edwin Ruiz <edwinruizr1992@gmail.com>
2018-04-29 21:49:59 -0700

    corrected results component

--- client/app.js
+++ client/app.js
@@ -1,27 +1,38 @@
+Vue.config.devtools = true
+
 // Chat Component
 const resultsComponent = {
-    template: ` <div>
-                  <ul class="collapsible">
-                  <li v-for="data in results">
-                    <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                  </li>
-                  </ul>
-               </div>`,
-    props: ['results']
+    template: `<div class="accordion" id="accordion">
+                <div class="card" v-for="(result, index) in data">
+                  <div class="card-header" id="headingindex">
+                    <h5 class="mb-0">
+                      <button class="btn btn-link" type="button" data-toggle="collapse" v-bind:data-target="'#' + index">
+                        {{result.title}}
+                      </button>
+                    </h5>
+                  </div>
+
+                  <div v-bind:id="index" class="collapse show" aria-labelledby="heading{{index}}" data-parent="#accordion">
+                    <div class="card-body">
+                      {{result.description}}
+                    </div>
+                  </div>
+                </div>
+              </div>`,
+    props: ['data']
 }
 
 const searchButtonComponent = {
-  template: `<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>`
+  template: `<div class="input-group">
+              <input type="text" class="form-control" aria-label="Text input with dropdown button">
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Type of Search</button>
+                <div class="dropdown-menu">
+                  <button class="dropdown-item" v-for="button in buttons" href="#">{{button}}</button>
+                </div>
+              </div>
+            </div>`,
+props: ['buttons']
 }
 
 //
@@ -30,8 +41,9 @@ const app = new Vue({
     el: '#reddit-app',
     data: {
         search: '',
-        results: ['test'],
-        previousSearches: []
+        results: [{title:"Result", description:"Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."},{title:"Resultado", description:"Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."}],
+        previousSearches: [],
+        searchButtons: ['Search Subreddits', 'Search User']
     },
     methods: {
         // search: function () {
